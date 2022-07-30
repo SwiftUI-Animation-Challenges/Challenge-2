e9569d15121b9a6fdbdf0fa731f1784359fdd111
sahil-sahu-dev <sahilsahucode@gmail.com>
2022-07-30 13:19:05 +0530

    Sahil Sahu submission

---
+++ Sahil Sahu Submission/AirdropAnimation/AirdropAnimation/AirdropAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirdropAnimationApp.swift
+//  AirdropAnimation
+//
+//  Created by Sahil Sahu on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AirdropAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(vm: ContentViewModel())
+        }
+    }
+}
